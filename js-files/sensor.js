77ef7bf2274429955bfd281163073fa16210688e
HerbertNtim <herbertntim15@gmail.com>
2025-07-22 16:24:06 +0000

    traffic simulation

--- js-files/sensor.js
+++ js-files/sensor.js
@@ -9,18 +9,19 @@ class Sensor {
     this.readings = [];
   }
 
-  update(roadBorders) {
+  update(roadBorders, traffic) {
     this.#castRays();
     this.readings = [];
     for (let i = 0; i < this.rays.length; i++) {
-      this.readings.push(this.#getReadings(this.rays[i], roadBorders));
+      this.readings.push(this.#getReadings(this.rays[i], roadBorders, traffic));
     }
     // console.log(this.readings)
   }
 
-  #getReadings(ray, roadBorders) {
+  #getReadings(ray, roadBorders, traffic) {
     let touches = [];
 
+    // Borders crash with road
     for (let i = 0; i < roadBorders.length; i++) {
       const touch = getIntersection(
         ray[0],
@@ -30,6 +31,23 @@ class Sensor {
       );
       if (touch) {
         touches.push(touch);
+      }
+    }
+
+    // Traffic Crash
+    for (let i = 0; i < traffic.length; i++) {
+      const poly = traffic[i].polygon;
+      for (let j = 0; j < poly.length; j++) {
+        const value = getIntersection(
+          ray[0],
+          ray[1],
+          poly[j],
+          poly[(j + 1) % poly.length]
+        );
+
+        if(value) {
+          touches.push(value)
+        }
       }
     }
 
